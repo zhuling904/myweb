9e1b6373cf837cdc8de5124b067a0f5079f12f39
zhuling822 <ling.zhu@js.design>
2023-06-20 15:32:18 +0800

    feat: 引入antd

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,11 @@
+import About from "../pages/About";
 import Home from "../pages/Home";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 const App = () => {
     return <>
        <Routes>
           <Route path="/home" element={<Home />}></Route>
-          <Route path="/about" element={'123'}></Route>
+          <Route path="/about" element={<About />}></Route>
         </Routes>
     </>
 }
