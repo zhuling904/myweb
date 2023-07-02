16c289f711397c772c4d33ceaca01d3dcfcfa9ab
zhuling822 <ling.zhu@js.design>
2023-07-02 11:50:20 +0800

    fix: style

---
+++ src/pages/Timeline/constants.tsx
@@ -0,0 +1,59 @@
+// eslint-disable-next-line react-refresh/only-export-components
+export const TIMELINES = [{
+    label: '2015-09-01132132132132',
+    color: 'black',
+    children: 'Create a services site 2015-09-01',
+}, {
+    label: '2015-09-01',
+    color: 'black',
+    children: 'Create a services site 2015-09-01',
+},
+{
+    label: '2015-09-01',
+    color: 'black',
+    children: (
+        <>
+            <p>Solve initial network problems 1</p>
+            <p>Solve initial network problems 2</p>
+            <p>Solve initial network problems 3 2015-09-01</p>
+        </>
+    ),
+},
+{
+    label: '2015-09-01',
+    color: 'black',
+    children: (
+        <>
+            <p>Technical testing 1</p>
+            <p>Technical testing 2</p>
+            <p>Technical testing 3 2015-09-01</p>
+        </>
+    ),
+},
+{
+    label: '2015-09-01',
+    color: 'black',
+    children: (
+        <>
+            <p>Technical testing 1</p>
+            <p>Technical testing 2</p>
+            <p>Technical testing 3 2015-09-01</p>
+        </>
+    ),
+},
+{
+    label: '2015-09-01',
+    color: 'black',
+    children: (
+        <>
+            <p>Technical testing 1</p>
+            <p>Technical testing 2</p>
+            <p>Technical testing 3 2015-09-01</p>
+        </>
+    ),
+},
+{
+    label: '2015-09-01',
+    color: 'black',
+    children: <p>Custom color testing</p>,
+},]
--- src/pages/Timeline/index.tsx
+++ src/pages/Timeline/index.tsx
@@ -1,4 +1,5 @@
 
+import { TIMELINES } from './constants';
 import style from './style/index.module.less';
 import { Timeline } from 'antd';
 const TimeLine = () => {
@@ -6,67 +7,7 @@ const TimeLine = () => {
         <div className={style.container}>
             <Timeline
                 mode='left'
-                items={[
-                    {
-                        label: '2015-09-01',
-                        color: 'gray',
-                        children: 'Create a services site 2015-09-01',
-                    },
-                    {
-                        label: '2015-09-01',
-                        color: 'gray',
-                        children: 'Create a services site 2015-09-01',
-                    },
-                    {
-                        label: '2015-09-01',
-                        color: 'gray',
-                        children: (
-                            <>
-                                <p>Solve initial network problems 1</p>
-                                <p>Solve initial network problems 2</p>
-                                <p>Solve initial network problems 3 2015-09-01</p>
-                            </>
-                        ),
-                    },
-                    {
-                        label: '2015-09-01',
-                        color: 'gray',
-                        children: (
-                            <>
-                                <p>Technical testing 1</p>
-                                <p>Technical testing 2</p>
-                                <p>Technical testing 3 2015-09-01</p>
-                            </>
-                        ),
-                    },
-                    {
-                        label: '2015-09-01',
-                        color: 'gray',
-                        children: (
-                            <>
-                                <p>Technical testing 1</p>
-                                <p>Technical testing 2</p>
-                                <p>Technical testing 3 2015-09-01</p>
-                            </>
-                        ),
-                    },
-                    {
-                        label: '2015-09-01',
-                        color: 'gray',
-                        children: (
-                            <>
-                                <p>Technical testing 1</p>
-                                <p>Technical testing 2</p>
-                                <p>Technical testing 3 2015-09-01</p>
-                            </>
-                        ),
-                    },
-                    {
-                        label: '2015-09-01',
-                        color: 'gray',
-                        children: <p>Custom color testing</p>,
-                    },
-                ]}
+                items={TIMELINES}
             />
         </div>
     </>
