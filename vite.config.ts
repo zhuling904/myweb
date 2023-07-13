d294e82445931b39c0a5e64733f8755102ed579a
zhuling822 <ling.zhu@js.design>
2023-07-13 20:09:51 +0800

    fix: 更换入口文件位置

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname, 'src'),
   css: {
     //* css模块化
     modules: { // css模块化 文件以.module.[css|less|scss]结尾
