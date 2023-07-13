d710cb8047c20c23594f5c4454a8efba7d46f8d4
zhuling822 <ling.zhu@js.design>
2023-07-13 20:27:56 +0800

    feat: autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import autoprefixer from 'autoprefixer';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   root: path.resolve(__dirname, 'src'),
   css: {
+    // 进行 PostCSS 配置
+    postcss: {
+      plugins: [
+        autoprefixer({
+          // 指定目标浏览器
+          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11']
+        })
+      ]
+    },
     //* css模块化
     modules: { // css模块化 文件以.module.[css|less|scss]结尾
       generateScopedName: '[name]__[local]___[hash:base64:5]',
