e13b2f19cffcfd8489e8c5485a36155d9eec3ec4
zhuling904 <ling.zhu.904@gmail.com>
2023-12-17 15:08:31 +0800

    style: 样式更改

--- src/pages/Works/components/WorkCard/index.tsx
+++ src/pages/Works/components/WorkCard/index.tsx
@@ -1,27 +1,30 @@
-import style from './style/index.module.less';
+import style from "./style/index.module.less";
 interface WorkCardProps {
-    desc: string, // 作品秒速
-    link: string, // 作品链接
-    coverImg: string, // 封面图
-    miniImg: string,  // 小图
+  desc: string; // 作品秒速
+  link: string; // 作品链接
+  coverImg: string; // 封面图
+  miniImg: string; // 小图
 }
 const WorkCard = (props: WorkCardProps) => {
-    const { desc, link, coverImg, miniImg } = props;
-    return <div className={style.container}
-        onClick={() => { window.open(link, "_blank") }}
+  const { desc, link, coverImg, miniImg } = props;
+  return (
+    <div
+      className={style.container}
+      onClick={() => {
+        window.open(link, "_blank");
+      }}
     >
-        <div className={style.top}>
-            <img src={coverImg} alt="" />
+      <div className={style.top}>
+        <img src={coverImg} alt="" />
+      </div>
+      <div className={style.bottom}>
+        <div className={style.img}>
+          <img src={miniImg} alt="" />
         </div>
-        <div className={style.bottom}>
-            <div className={style.img}>
-                <img src={miniImg} alt="" />
-            </div>
-            <div className={style.desc}>
-                {desc}
-            </div>
-        </div>
+        <div className={style.desc}>{desc}</div>
+      </div>
     </div>
-}
+  );
+};
 
 export default WorkCard;
